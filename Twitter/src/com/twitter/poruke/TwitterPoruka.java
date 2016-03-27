6f8e0b9900bea32c6d6f05b0f841a75159f5a0bf
dusanstankovic1995 <dukki95@gmail.com>
2016-03-28 01:59:14 +0200

    Uradjena kompletna dokumentacija za klasu TwitterPoruka.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,13 +1,43 @@
 package com.twitter.poruke;
 
-public class TwitterPoruka {
+/**
+ * Ovo je klasa koja koja sadrzi dva atributa:
+ * <ul>
+ * <li>korisnik</li>
+ * <li>poruka</li>
+ * </ul>
+ * i predstavlja poruku na tviteru.
+ * 
+ * @author Dusan Stankovic
+ *
+ */
 
+public class TwitterPoruka {
+	/**
+	 * Atribut korisnik predstavlja ime korisnika.
+	 */
 	private String korisnik;
+	/**
+	 * Atribut poruka predstavlja sadrzaj poruke.
+	 */
 	private String poruka;
+
+	/**
+	 * Ova metoda vraca ime korisnika.
+	 * 
+	 * @return ime korisnika
+	 */
 
 	public String getKorisnik() {
 		return korisnik;
 	}
+
+	/**
+	 * Ova metoda postavlja vrednost atributa korisnik na datog korisnika.
+	 * 
+	 * @param korisnik
+	 *            ime korisnika
+	 */
 
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik == "")
@@ -15,15 +45,32 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Ova metoda vraca odgovarajucu poruku.
+	 * 
+	 * @return odgovarajuca poruka
+	 */
+
 	public String getPoruka() {
 		return "poruka";
 	}
+
+	/**
+	 * Ova metoda postavlja vrednost atributa poruka na unetu poruku.
+	 * 
+	 * @param poruka
+	 *            odgovarajuca poruka
+	 */
 
 	public void setPoruka(String poruka) {
 		if (poruka == null || this.poruka.length() > 140)
 			throw new RuntimeException("Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
+
+	/**
+	 * Ova metoda vraca vrednosti atributa korisnik i atributa poruka.
+	 */
 
 	public String toString() {
 		return "KORISNIK:" + korisnik + " PORUKA:" + poruka;
